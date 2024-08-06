6bd275fcd4974ecc1d4af22d21cf3be0d4f65e6d
Wanying Zhu <wanying.zhu.1@vanderbilt.edu>
2024-08-06 10:34:37 -0500

    add imputation feature

--- prs_from_vcf/get_weighted_dosage.py
+++ prs_from_vcf/get_weighted_dosage.py
@@ -6,17 +6,6 @@ Flip dosage as 2-old_dosage if effect allele is not alt allele
 Save processed flipped_dosage*weight in output file
 
 Example call:
-method=PT; lipid=HDL; chromosome=21; ancestry=HIS
-output_fn=/data100t1/home/wanying/BioVU/202405_PAGE_PRS/outputs/redo/flipped_dosage_weighted/${ancestry}/${lipid}/${lipid}_${method}_chr${chromosome}.weighted_dosage
-python /data100t1/home/wanying/BioVU/202405_PAGE_PRS/code/utils/get_weighted_dosage.py \
---lipid ${lipid} \
---method ${method} \
---chromosome ${chromosome} \
---ancestry ${ancestry} \
---vcf /data100t1/share/BioVU/TOPMed_imputed/${ancestry}/chr${chromosome}.dose.vcf.gz \
---snp_list /data100t1/home/wanying/BioVU/202405_PAGE_PRS/supporting_files/redo/${lipid}/${method}/chr${chromosome}_snp.list \
---output_fn ${output_fn}
-
 python /data100t1/home/wanying/BioVU/202405_PAGE_PRS/code/utils/get_weighted_dosage.py \
 --lipid HDL \
 --method PT \
@@ -24,12 +13,15 @@ python /data100t1/home/wanying/BioVU/202405_PAGE_PRS/code/utils/get_weighted_dos
 --ancestry EUR \
 --vcf /data100t1/share/BioVU/TOPMed_imputed/remerged_EUR/merged_chr1.vcf.gz \
 --snp_list /data100t1/home/wanying/BioVU/202405_PAGE_PRS/supporting_files/redo/HDL/PT/chr1_snp.list \
---output_fn /data100t1/home/wanying/BioVU/202405_PAGE_PRS/outputs/redo/flipped_dosage_weighted/EUR/HDL/HDL_PT_chr1.weighted_dosage
-
+--output_fn /data100t1/home/wanying/BioVU/202405_PAGE_PRS/outputs/redo/flipped_dosage_weighted/EUR/HDL/HDL_PT_chr1.weighted_dosage \
+--impute \
+--mean_af_col_anc MeanAF_European \
+--mean_af_col MeanAF
 '''
 
 import pandas as pd
 import numpy as np
+import sys
 import subprocess
 import os
 import gzip
@@ -63,9 +55,18 @@ def process_args():
     parser.add_argument('--ancestry', default='EUR', type=str, choices=['AFR', 'EAS', 'EUR', 'HIS', 'SAS'])
     parser.add_argument('--vcf', type=str, default='/data100t1/share/BioVU/TOPMed_imputed',
                         help='VCF file to look up SNPs')
-    parser.add_argument('--snp_list', type=str, default='/data100t1/home/wanying/BioVU/202405_PAGE_PRS/supporting_files/redo/HDL/PT/chr22_snp.list',
+    parser.add_argument('--snp_list', type=str,
+                        default='/data100t1/home/wanying/BioVU/202405_PAGE_PRS/supporting_files/redo/HDL/PT/chr22_snp.list',
                         help='File name of SNP list with pos, alt, ref, effect allele and PRS weight')
     parser.add_argument('--output_fn', type=str)
+    parser.add_argument('--impute', action='store_true',
+                        help='Impute missing SNPs using mean allele count (= mean allele frequency * 2)')
+    parser.add_argument('--mean_af_col_anc', type=str, default=None,
+                        help='Column name of ancestry specific mean allele frequency (for imputation). Such as MeanAF_European.')
+    parser.add_argument('--mean_af_col', type=str, default='MeanAF',
+                        help='Column name of mean allele frequency (for imputation). Use this one if ancestry-specific AF is missing')
+    parser.add_argument('--overwrite', action='store_true',
+                        help='Overwrite existing BGzipped file')
     args = parser.parse_args()
     output_path = os.path.split(args.output_fn)[0]
     if not os.path.isdir(output_path): # Create output folder if not exists
@@ -117,7 +118,7 @@ def process_dosage_of_single_snp(line, ref, alt, effect_allele):
             flip=True
     else:
         # A SNP at the given position is found, but not the one in the list
-        # Label the SNP as not not found
+        # Label the SNP as not found
         return None
         
     if flip: array_dosage = 2-array_dosage
@@ -140,7 +141,8 @@ def process_multiallelic(lines, pos, ref, alt):
                 return line
     return ''
 
-def calcualte_weighted_dosage(vcf_fn, df_snp_list, output_fn):
+def calcualte_weighted_dosage(vcf_fn, df_snp_list, output_fn, impute=False,
+                              mean_af_col_anc=None, mean_af_col=None):
     '''
     Takes in a snp list with flag indicates if the dosage needs to be flipped
     Save weight*dosage to an output file
@@ -148,10 +150,14 @@ def calcualte_weighted_dosage(vcf_fn, df_snp_list, output_fn):
     - dosage_fn: Dosage vcf to look up snps
     - df_snp_list: a dataframe of snps to look for, with snp ids, positions, ref, alt, effect allele, weights
     - output_fn: output file to save processes dosage*weight
+    - impute: impute missing SNPs using mean allele frequency if true
+    - mean_af_col_anc: mean allele frequency in current ancestral group samples (for imputation)
+    - mean_af_col: mean allele frequency in all samples (for imputation)
     '''
     not_found_fn = output_fn + '.not_found' # Save any snps that are missing from the dosage vcf
     out_fh = open(output_fn, 'w')
     not_found_fh = open(not_found_fn, 'w')
+    not_found_fh.write('ID\timputed_weighted_dosage\n')
     
     # Get header of dosage file
     with gzip.open(vcf_fn, 'rt') as fh:
@@ -171,12 +177,21 @@ def calcualte_weighted_dosage(vcf_fn, df_snp_list, output_fn):
         alt = df_snp_list['alt'].iloc[i]
         effect_allele = df_snp_list['PRS_effect_allele'].iloc[i]
         weight = df_snp_list['PRS_weight'].iloc[i]
-            
+
+        if impute:
+            mean_af = df_snp_list[mean_af_col_anc].iloc[i] # Mean allele frequency of current ancestral group
+            if np.isnan(mean_af): # If ancestry specific allele frequency is missing, use the mean AF instead
+                mean_af = df_snp_list[mean_af_col].iloc[i] # Mean allele frequency in all samples
+            w_dosage = mean_af*2 * weight # use mean_af*2 as imputed dosage
+        
         cmd = f'tabix {vcf_fn} {chr_num}:{pos}-{pos}'
         result = subprocess.run(cmd, shell=True, capture_output=True, text=True).stdout.rstrip()
         if result == '':
             count_not_found += 1
-            not_found_fh.write(snp+'\n')
+            if impute:
+                not_found_fh.write(f'{snp}\t{w_dosage}\n')
+            else:
+                not_found_fh.write(f'{snp}\tNA\n')
             continue # If not found
 
         lines = result.split('\n')
@@ -191,7 +206,12 @@ def calcualte_weighted_dosage(vcf_fn, df_snp_list, output_fn):
                                                           effect_allele=effect_allele)
             else: # No match
                 count_not_found += 1
-                not_found_fh.write(snp+'\n')
+                # not_found_fh.write(snp+'\n')
+
+                if impute:
+                    not_found_fh.write(f'{snp}\t{w_dosage}\n')
+                else:
+                    not_found_fh.write(f'{snp}\tNA\n')
                 continue
         else:
             # Get dosage
@@ -202,7 +222,10 @@ def calcualte_weighted_dosage(vcf_fn, df_snp_list, output_fn):
         else:
             # If lst_dosage is none, exclude the SNP from PRS and label it as missing
             count_not_found += 1
-            not_found_fh.write(snp+'\n')
+            if impute:
+                not_found_fh.write(f'{snp}\t{w_dosage}\n')
+            else:
+                not_found_fh.write(f'{snp}\tNA\n')
             
         if c_total>10000 and c_total%100==0:
             print(f'\r# Processed: {c_total}/{len(df_snp_list)}    ', end='', flush=True)
@@ -228,15 +251,36 @@ for arg in vars(args):
     logging.info(msg)
 
 logging.info('\n# Call used:')
-logging.info(cmd_used+'\n')
+logging.info('python ' + ' '.join(sys.argv))
+
+if os.path.isfile(args.output_fn+'.gz'):
+    if not args.overwrite:
+        logging.info('# BGzipped output file already exists. Skip')
+        exit()
+    else:
+        # Remove existing BGzipped file and tabix file
+        subprocess.run(f'rm {args.output_fn}.gz', shell=True)
+        subprocess.run(f'rm {args.output_fn}.gz.tbi', shell=True)
 
 # ##################### Load files ####################
 df_snp_list = pd.read_csv(args.snp_list, sep='\t', dtype={'pos':int})
 
+if args.impute: # Ignore SNPs without mean allele frequencies (cannot be imputed)
+    # Save SNPs that cannot be imputed
+    fn_cannot_impute = args.output_fn + '.cannot_impute'
+    df_snp_list[df_snp_list['MeanAF'].isna()].to_csv(fn_cannot_impute, index=False)
+    df_snp_list = df_snp_list[df_snp_list['MeanAF'].notna()].copy()
+
 calcualte_weighted_dosage(args.vcf,
                           df_snp_list.sort_values(by='pos'),
-                          args.output_fn)
+                          args.output_fn, impute=args.impute,
+                          mean_af_col_anc=args.mean_af_col_anc,
+                          mean_af_col=args.mean_af_col)
 
-logging.info('# gzip flipped dosage')
-subprocess.run(f'gzip {args.output_fn}', shell=True)
-logging.info('\n#DONE\n')
+logging.info('# bgzip flipped dosage')
+subprocess.run(f'bgzip {args.output_fn}', shell=True)
+
+logging.info('# tabix flipped dosage')
+subprocess.run(f'tabix -b 2 -e 2 {args.output_fn}.gz', shell=True)
+
+logging.info('\n# DONE\n')
